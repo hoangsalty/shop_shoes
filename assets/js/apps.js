cdd80102597225633e7c748f09d34f7f4ac2eac8
Cuongg1 <0306191011@caothang.edu.vn>
2023-05-17 22:31:34 +0700

    commit

--- assets/js/apps.js
+++ assets/js/apps.js
@@ -1,61 +1,69 @@
 FRAMEWORK.PopupLogin = function () {
-  $('#form-user').submit(function (e) {
+  $("#form-user").submit(function (e) {
     e.preventDefault();
 
-    var username = $(this).find('#username');
-    var password = $(this).find('#password');
+    var username = $(this).find("#username");
+    var password = $(this).find("#password");
     if (isEmpty(username.val())) {
-      $('.login_response').html('<div class="alert alert-danger">Vui lòng nhập tài khoản</div>');
+      $(".login_response").html(
+        '<div class="alert alert-danger">Vui lòng nhập tài khoản</div>'
+      );
       username.focus();
       return false;
     }
     if (isEmpty(password.val())) {
-      $('.login_response').html('<div class="alert alert-danger">Vui lòng nhập mật khẩu</div>');
+      $(".login_response").html(
+        '<div class="alert alert-danger">Vui lòng nhập mật khẩu</div>'
+      );
       password.focus();
       return false;
     }
 
     $.ajax({
-      url: 'api/login.php',
-      type: 'POST',
-      dataType: 'json',
+      url: "api/login.php",
+      type: "POST",
+      dataType: "json",
       data: {
         username: username.val(),
         password: password.val(),
       },
       beforeSend: function () {
         holdonOpen();
-        $('#popup-login').find('.modal-body').css('opacity', '0.5');
-        $(this).find('.login-account').prop('disabled', true);
+        $("#popup-login").find(".modal-body").css("opacity", "0.5");
+        $(this).find(".login-account").prop("disabled", true);
       },
       success: function (result) {
-        $('#popup-login').find('.modal-body').css('opacity', '1');
-        $(this).find('.login-account').prop('disabled', false);
+        $("#popup-login").find(".modal-body").css("opacity", "1");
+        $(this).find(".login-account").prop("disabled", false);
 
         if (result.status == 200) {
           $("#form-user")[0].reset();
-          $('.login_response').html('<div class="alert alert-success">' + result.message + '</div>');
+          $(".login_response").html(
+            '<div class="alert alert-success">' + result.message + "</div>"
+          );
 
           setTimeout(function () {
             location.reload();
           }, 1000);
         } else if (result.status == 404) {
-          $('.login_response').html('<div class="alert alert-danger">' + result.message + '</div>');
+          $(".login_response").html(
+            '<div class="alert alert-danger">' + result.message + "</div>"
+          );
         }
         holdonClose();
-      }
+      },
     });
   });
-}
+};
 
 FRAMEWORK.Random = function () {
-  $('.birth-date').datetimepicker({
+  $(".birth-date").datetimepicker({
     timepicker: false,
-    format: 'd/m/Y',
-    formatDate: 'd/m/Y',
+    format: "d/m/Y",
+    formatDate: "d/m/Y",
     maxDate: TIMENOW,
   });
-}
+};
 
 FRAMEWORK.Menu = function () {
   /* Menu remove empty ul */
@@ -84,14 +92,12 @@ FRAMEWORK.Menu = function () {
         $(".header").height() + $(".slideshow").height()
       ) {
         $(".menu").addClass("fixed animate__fadeInDown animate__animated");
-        $(".ul-menu").addClass("hidden-menu");
         $(".header").css({ "margin-bottom": $(".menu").height() });
+        $(".ul-menu").addClass("hidden-menu");
       } else {
         $(".menu").removeClass("fixed animate__fadeInDown animate__animated");
         $(".header").css({ "margin-bottom": "" });
-        if (isExist($(".wrap-home"))) {
-          $(".ul-menu").removeClass("hidden-menu");
-        }
+        $(".ul-menu").removeClass("hidden-menu");
       }
     } else {
       if ($(window).scrollTop() >= $(".header").height()) {
@@ -125,7 +131,7 @@ FRAMEWORK.Menu = function () {
 
     $(".ul-menu").height(heigth);
   });
-}
+};
 
 FRAMEWORK.Search = function () {
   if (isExist($(".icon-search"))) {
@@ -145,30 +151,64 @@ FRAMEWORK.Search = function () {
       $(".icon-search i").toggleClass("fa fa-search fa fa-times");
     });
   }
-}
+};
 
 FRAMEWORK.Carousel = function () {
-  $('.slick_more_product .boxProduct').slick({
-    lazyLoad: 'progressive', infinite: true, accessibility: true, vertical: false, verticalSwiping: false,
-    slidesToShow: 4, slidesToScroll: 1, autoplay: true, autoplaySpeed: 2000, speed: 1000, arrows: false,
-    centerMode: false, dots: false, draggable: true, responsive: [
-    ]
+  $(".slick_more_product .boxProduct").slick({
+    lazyLoad: "progressive",
+    infinite: true,
+    accessibility: true,
+    vertical: false,
+    verticalSwiping: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    speed: 1000,
+    arrows: false,
+    centerMode: false,
+    dots: false,
+    draggable: true,
+    responsive: [],
   });
 
-  $('.product__details__pic__left').slick({
-    lazyLoad: 'progressive', infinite: true, accessibility: true, vertical: false, verticalSwiping: false,
-    slidesToShow: 5, slidesToScroll: 1, autoplay: true, autoplaySpeed: 2000, speed: 1000, arrows: false,
-    asNavFor: '.product__details__pic__right', focusOnSelect: true,
-    centerMode: false, dots: false, draggable: true, responsive: [
-    ]
+  $(".product__details__pic__left").slick({
+    lazyLoad: "progressive",
+    infinite: true,
+    accessibility: true,
+    vertical: false,
+    verticalSwiping: false,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    speed: 1000,
+    arrows: false,
+    asNavFor: ".product__details__pic__right",
+    focusOnSelect: true,
+    centerMode: false,
+    dots: false,
+    draggable: true,
+    responsive: [],
   });
 
-  $('.product__details__pic__right').slick({
-    lazyLoad: 'progressive', infinite: true, accessibility: true, vertical: false, verticalSwiping: false,
-    slidesToShow: 1, slidesToScroll: 1, autoplay: false, autoplaySpeed: 2000, speed: 1000, arrows: false,
-    asNavFor: '.product__details__pic__left',
-    centerMode: false, dots: false, draggable: true, responsive: [
-    ]
+  $(".product__details__pic__right").slick({
+    lazyLoad: "progressive",
+    infinite: true,
+    accessibility: true,
+    vertical: false,
+    verticalSwiping: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    speed: 1000,
+    arrows: false,
+    asNavFor: ".product__details__pic__left",
+    centerMode: false,
+    dots: false,
+    draggable: true,
+    responsive: [],
   });
 
   $(".slideshow__slider").owlCarousel({
@@ -255,7 +295,7 @@ FRAMEWORK.Carousel = function () {
       },
     },
   });
-}
+};
 
 FRAMEWORK.Pagings = function () {
   loadPaging("api/product.php?perpage=8", ".page_splist", 0);
@@ -263,9 +303,13 @@ FRAMEWORK.Pagings = function () {
     $(".list_splist a").removeClass("active");
     $(this).addClass("active");
     var idList = $(this).data("list");
-    loadPaging("api/product.php?idList=" + idList + "&perpage=8", ".page_splist", 0);
+    loadPaging(
+      "api/product.php?idList=" + idList + "&perpage=8",
+      ".page_splist",
+      0
+    );
   });
-}
+};
 
 FRAMEWORK.Bootstrap = function () {
   if (isExist($("[data-margin]"))) {
@@ -298,58 +342,64 @@ FRAMEWORK.Bootstrap = function () {
       });
     });
   }
-}
+};
 
 FRAMEWORK.Cart = function () {
   /* Popup */
-  $('body').on('click', '.popup_cart', function () {
+  $("body").on("click", ".popup_cart", function () {
     $.ajax({
-      url: 'api/cart.php',
-      type: 'POST',
-      dataType: 'html',
+      url: "api/cart.php",
+      type: "POST",
+      dataType: "html",
       data: {
-        cmd: 'popup-cart'
+        cmd: "popup-cart",
       },
       success: function (result) {
-        $('#popup-cart .modal-body').html(result);
-        $('#popup-cart').modal('show');
+        $("#popup-cart .modal-body").html(result);
+        $("#popup-cart").modal("show");
         holdonClose();
-      }
+      },
     });
   });
   /* Add */
-  $('body').on('click', '.btn-cart', function () {
+  $("body").on("click", ".btn-cart", function () {
     if (!IS_LOGIN) {
-      confirmDialog('force-login', 'Vui lòng đăng nhập');
+      confirmDialog("force-login", "Vui lòng đăng nhập");
       return false;
     }
   });
-  $('body').on('click', '.addcart', function () {
+  $("body").on("click", ".addcart", function () {
     if (!IS_LOGIN) {
-      confirmDialog('force-login', 'Vui lòng đăng nhập');
+      confirmDialog("force-login", "Vui lòng đăng nhập");
       return false;
     }
 
     $this = $(this);
-    $parents = $this.parents('.right-pro-detail');
-    var id = $this.data('id');
-    var action = $this.data('action');
-    var quantity = $parents.find('.quantity-pro-detail').find('.qty-pro').val();
+    $parents = $this.parents(".right-pro-detail");
+    var id = $this.data("id");
+    var action = $this.data("action");
+    var quantity = $parents.find(".quantity-pro-detail").find(".qty-pro").val();
     quantity = quantity ? quantity : 1;
 
     /* size màu*/
-    var color = $parents.find('.color-block-pro-detail').find('.color-pro-detail input:checked').val();
+    var color = $parents
+      .find(".color-block-pro-detail")
+      .find(".color-pro-detail input:checked")
+      .val();
     color = color ? color : 0;
-    var size = $parents.find('.size-block-pro-detail').find('.size-pro-detail input:checked').val();
+    var size = $parents
+      .find(".size-block-pro-detail")
+      .find(".size-pro-detail input:checked")
+      .val();
     size = size ? size : 0;
 
     if (id) {
       $.ajax({
-        url: 'api/cart.php',
-        type: 'POST',
-        dataType: 'json',
+        url: "api/cart.php",
+        type: "POST",
+        dataType: "json",
         data: {
-          cmd: 'add-cart',
+          cmd: "add-cart",
           id: id,
           color: color,
           size: size,
@@ -359,103 +409,119 @@ FRAMEWORK.Cart = function () {
           holdonOpen();
         },
         success: function (result) {
-          if (action == 'addnow') {
-            $('.count-cart').html(result.max);
+          if (action == "addnow") {
+            $(".count-cart").html(result.max);
 
             $.ajax({
-              url: 'api/cart.php',
-              type: 'POST',
-              dataType: 'html',
+              url: "api/cart.php",
+              type: "POST",
+              dataType: "html",
               data: {
-                cmd: 'popup-cart'
+                cmd: "popup-cart",
               },
               beforeSend: function () {
                 holdonOpen();
               },
               success: function (result) {
-                $('#popup-cart .modal-body').html(result);
-                $('#popup-cart').modal('show');
-                $('#popup-quickview').modal('hide');
+                $("#popup-cart .modal-body").html(result);
+                $("#popup-cart").modal("show");
+                $("#popup-quickview").modal("hide");
                 holdonClose();
-              }
+              },
             });
-          } else if (action == 'buynow') {
-            window.location = CONFIG_BASE + 'gio-hang';
+          } else if (action == "buynow") {
+            window.location = CONFIG_BASE + "gio-hang";
           }
-        }
+        },
       });
     }
   });
   /* Delete */
-  $('body').on('click', '.del-procart', function () {
-    confirmDialog('delete-procart', 'Bạn muốn xóa sản phẩm này khỏi giỏ hàng ?', $(this));
+  $("body").on("click", ".del-procart", function () {
+    confirmDialog(
+      "delete-procart",
+      "Bạn muốn xóa sản phẩm này khỏi giỏ hàng ?",
+      $(this)
+    );
   });
   /* Counter */
-  $('body').on('click', '.counter-procart', function () {
+  $("body").on("click", ".counter-procart", function () {
     var $button = $(this);
     var quantity = 1;
-    var input = $button.parent().find('input');
-    var id = input.data('pid');
-    var code = input.data('code');
-    var oldValue = $button.parent().find('input').val();
-    if ($button.text() == '+') quantity = parseFloat(oldValue) + 1;
+    var input = $button.parent().find("input");
+    var id = input.data("pid");
+    var code = input.data("code");
+    var oldValue = $button.parent().find("input").val();
+    if ($button.text() == "+") quantity = parseFloat(oldValue) + 1;
     else if (oldValue > 1) quantity = parseFloat(oldValue) - 1;
-    $button.parent().find('input').val(quantity);
+    $button.parent().find("input").val(quantity);
     updateCart(id, code, quantity);
   });
   /* City */
-  if (isExist($('.select-city-cart'))) {
-    $('.select-city-cart').change(function () {
+  if (isExist($(".select-city-cart"))) {
+    $(".select-city-cart").change(function () {
       var id = $(this).val();
       loadDistrict(id);
     });
   }
   /* District */
-  if (isExist($('.select-district-cart'))) {
-    $('.select-district-cart').change(function () {
+  if (isExist($(".select-district-cart"))) {
+    $(".select-district-cart").change(function () {
       var id = $(this).val();
       loadWard(id);
     });
   }
   /* Ward */
-  if (isExist($('.select-ward-cart'))) {
-    $('.select-ward-cart').change(function () {
+  if (isExist($(".select-ward-cart"))) {
+    $(".select-ward-cart").change(function () {
       var id = $(this).val();
     });
   }
   /* Payments */
-  if (isExist($('.payments-label'))) {
-    $('.payments-label').click(function () {
-      var payments = $(this).data('payments');
-      $('.payments-cart .payments-label, .payments-info').removeClass('active');
-      $(this).addClass('active');
-      $('.payments-info-' + payments).addClass('active');
+  if (isExist($(".payments-label"))) {
+    $(".payments-label").click(function () {
+      var payments = $(this).data("payments");
+      $(".payments-cart .payments-label, .payments-info").removeClass("active");
+      $(this).addClass("active");
+      $(".payments-info-" + payments).addClass("active");
     });
   }
   /* Colors */
-  if (isExist($('.color-pro-detail'))) {
-    $('.color-pro-detail input').click(function () {
-      $this = $(this).parents('label.color-pro-detail');
-      $parents = $this.parents('.attr-pro-detail');
-      $parents_detail = $this.parents('.grid-pro-detail');
-      $parents.find('.color-block-pro-detail').find('.color-pro-detail').removeClass('active');
-      $parents.find('.color-block-pro-detail').find('.color-pro-detail input').prop('checked', false);
-      $this.addClass('active');
-      $this.find('input').prop('checked', true);
+  if (isExist($(".color-pro-detail"))) {
+    $(".color-pro-detail input").click(function () {
+      $this = $(this).parents("label.color-pro-detail");
+      $parents = $this.parents(".attr-pro-detail");
+      $parents_detail = $this.parents(".grid-pro-detail");
+      $parents
+        .find(".color-block-pro-detail")
+        .find(".color-pro-detail")
+        .removeClass("active");
+      $parents
+        .find(".color-block-pro-detail")
+        .find(".color-pro-detail input")
+        .prop("checked", false);
+      $this.addClass("active");
+      $this.find("input").prop("checked", true);
     });
   }
   /* Sizes */
-  if (isExist($('.size-pro-detail'))) {
-    $('.size-pro-detail input').click(function () {
-      $this = $(this).parents('label.size-pro-detail');
-      $parents = $this.parents('.attr-pro-detail');
-      $parents.find('.size-block-pro-detail').find('.size-pro-detail').removeClass('active');
-      $parents.find('.size-block-pro-detail').find('.size-pro-detail input').prop('checked', false);
-      $this.addClass('active');
-      $this.find('input').prop('checked', true);
+  if (isExist($(".size-pro-detail"))) {
+    $(".size-pro-detail input").click(function () {
+      $this = $(this).parents("label.size-pro-detail");
+      $parents = $this.parents(".attr-pro-detail");
+      $parents
+        .find(".size-block-pro-detail")
+        .find(".size-pro-detail")
+        .removeClass("active");
+      $parents
+        .find(".size-block-pro-detail")
+        .find(".size-pro-detail input")
+        .prop("checked", false);
+      $this.addClass("active");
+      $this.find("input").prop("checked", true);
     });
   }
-}
+};
 
 /* Ready */
 $(document).ready(function () {
