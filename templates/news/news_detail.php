eea00a0dfd02d1929564f73694656421498ac812
Bé Bỏng <hoangsalty@gmail.com>
2023-07-08 22:34:23 +0700

    1

--- templates/news/news_detail.php
+++ templates/news/news_detail.php
@@ -17,7 +17,7 @@
                 <?php foreach ($news as $k => $v) { ?>
                     <a class="news_item d-flex align-items-center" href="<?= $v['slug'] ?>" title="<?= $v['name'] ?>">
                         <p class="pic-news scale-img">
-                            <?= $func->getImage(['class' => 'w-100', 'width' => 90, 'height' => 70, 'upload' => UPLOAD_NEWS_L, 'image' => $v['photo'], 'alt' => $v['name']]) ?>
+                            <?= $func->getImage(['class' => 'w-100', 'width' => 90, 'height' => 70, 'upload' => UPLOAD_NEWS_L, 'image' => $v['photo']]) ?>
                         </p>
                         <div class="info-news">
                             <h3 class="name-news text-split"><?= $v['name'] ?></h3>
